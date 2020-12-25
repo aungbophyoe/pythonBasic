f292ddcb097763686eb16e7a49665d9046952f6e
aungbophyoe <abbp1001@gmail.com>
2020-12-26 00:47:46 +0630

    wip

---
+++ main.py
@@ -0,0 +1,69 @@
+# print('Hello world')
+
+# num1 = 15432
+
+# num2 = 10
+
+# sum = num1 + num2
+# print(sum)
+
+# val = 10
+
+# v = ['apple','orange','u']
+# foundV = 'a'
+
+# if foundV in v :
+#     print('found it')
+# else :
+#     print('not found')    
+
+# charArr = "This is string, Hello , how r u?"
+# foundChar = "?"
+# if foundChar in charArr:
+#     print('found it')
+# else:
+#     print('not found')
+
+# def my_function():
+#     print('hello to my world')
+#     print("Hey ")
+
+# my_function()
+
+# def arg_function(name):
+#     print("Hello "+name)
+
+
+# arg_function('BO BO')    
+
+# def sum(num1,num2):
+#     return num1+num2
+
+# number1 = input('Enter first number : ')
+# number1 = int(number1)
+
+# number2 = input("Enter second number : ")
+# number2 = int(number2)
+
+# result = sum(number1,number2)
+# result = str(result)
+# print("Result is : "+result)    
+
+
+# a = ['aa','bb','cc']
+
+# print(list(a))
+# for x in a:
+#     print(x)
+
+# print(list(range(1,10)))
+
+# v = range(1,15)
+# for i in v:
+#     print(i)
+
+# b = [['apple','orange'],['2020','2021'],['czp','abp']]    
+
+# for n in b:
+#     for c in n:
+#         print(c)
